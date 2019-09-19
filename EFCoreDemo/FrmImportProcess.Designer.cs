a84a0549d9ec81e6641ae21b73ab61f1e0d8a12a
Administrator <Administrator@USER-20190317HU>
2019-09-19 23:06:42 +0800

    20190919

--- EFCoreDemo/FrmImportProcess.Designer.cs
+++ EFCoreDemo/FrmImportProcess.Designer.cs
@@ -41,6 +41,7 @@
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox1.Size = new System.Drawing.Size(776, 386);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
             // backgroundWorker1
             // 
--- EFCoreDemo/FrmImportProcess.cs
+++ EFCoreDemo/FrmImportProcess.cs
@@ -19,7 +19,7 @@ namespace EFCoreDemo
             backgroundWorker1.WorkerSupportsCancellation = true;
         }
 
-        private List<FeedbackBusiness> getFeedbackInfO(string fileName)
+        private bool getFeedbackInfo(string fileName)
         {
 
             XlsToDb xlsToDb = new XlsToDb();
@@ -28,114 +28,144 @@ namespace EFCoreDemo
             dt2 = dt.Clone();
             List<FeedbackBusiness> feedbackBusinesses = new List<FeedbackBusiness>();
             FeedbackBusiness feedbackBusiness;
-            for(int i = 1; i < dt.Rows.Count; i++)
+            try
             {
-                DataRow dr = dt.Rows[i];
-                feedbackBusiness = new FeedbackBusiness();
-                feedbackBusiness.FullName = dr[0].ToString();
-                feedbackBusiness.CardNo = dr[1].ToString();
-                feedbackBusiness.BusinessName = dr[2].ToString();
-                feedbackBusiness.BusinessType = dr[3].ToString();
-                if (!(dr[4] == null || dr[4].ToString().Length == 0))
+                backgroundWorker1.ReportProgress(0, "共检索到" + (dt.Rows.Count-1).ToString() + "家企业信息。\r\n\r\n");
+                for (int i = 1; i < dt.Rows.Count; i++)
                 {
-                    feedbackBusiness.EstDate = DateTime.Parse(dr[4].ToString());
+                    DataRow dr = dt.Rows[i];
+                    feedbackBusiness = new FeedbackBusiness();
+                    feedbackBusiness.FullName = dr[0].ToString();
+                    feedbackBusiness.CardNo = dr[1].ToString();
+                    feedbackBusiness.BusinessName = dr[2].ToString();
+                    backgroundWorker1.ReportProgress(0, "正在导入" + feedbackBusiness.BusinessName + "信息。\r\n\r\n");
+                    feedbackBusiness.BusinessType = dr[3].ToString();
+                    if (!(dr[4] == null || dr[4].ToString().Length == 0))
+                    {
+                        feedbackBusiness.EstDate = DateTime.Parse(dr[4].ToString());
+                    }
+                    else
+                    {
+                        feedbackBusiness.EstDate = DateTime.MinValue;
+                    }
+                    feedbackBusiness.LegalRep = dr[5].ToString();
+                    feedbackBusiness.Residence = dr[6].ToString();
+                    feedbackBusiness.State = dr[7].ToString();
+                    if (dr[8].ToString().Equals("是"))
+                    {
+                        feedbackBusiness.IsAbnormal = true;
+                    }
+                    else
+                    {
+                        feedbackBusiness.IsAbnormal = false;
+                    }
+                    if (dr[9].ToString().Equals("是"))
+                    {
+                        feedbackBusiness.IsOutrage = true;
+                    }
+                    else
+                    {
+                        feedbackBusiness.IsOutrage = false;
+                    }
+                    if (!(dr[10] == null || dr[10].ToString().Length == 0))
+                    {
+                        feedbackBusiness.RevokeDate = DateTime.Parse(dr[10].ToString());
+                    }
+                    else
+                    {
+                        feedbackBusiness.RevokeDate = DateTime.MinValue;
+                    }
+                    if (!(dr[11] == null || dr[11].ToString().Length == 0))
+                    {
+                        feedbackBusiness.LogoffDate = DateTime.Parse(dr[11].ToString());
+                    }
+                    else
+                    {
+                        feedbackBusiness.LogoffDate = DateTime.MinValue;
+                    }
+                    feedbackBusiness.CreditCode = dr[12].ToString();
+                    if (!(dr[13] == null || dr[13].ToString().Length == 0))
+                    {
+                        feedbackBusiness.RegisteredCapital = decimal.Parse(dr[13].ToString());
+                    }
+                    else
+                    {
+                        feedbackBusiness.RegisteredCapital = 0;
+                    }
+                    feedbackBusiness.Currency = dr[14].ToString();
+                    if (!(dr[15] == null || dr[15].ToString().Length == 0))
+                    {
+                        feedbackBusiness.Subscribe = decimal.Parse(dr[15].ToString());
+                    }
+                    else
+                    {
+                        feedbackBusiness.Subscribe = 0;
+                    }
+                    if (!(dr[16] == null || dr[16].ToString().Length == 0))
+                    {
+                        feedbackBusiness.Proportion = decimal.Parse(dr[16].ToString());
+                    }
+                    else
+                    {
+                        feedbackBusiness.Proportion = 0;
+                    }
+                    if (!(dr[17] == null || dr[17].ToString().Length == 0))
+                    {
+                        feedbackBusiness.PutupDate = DateTime.Parse(dr[17].ToString());
+                    }
+                    else
+                    {
+                        feedbackBusiness.PutupDate = DateTime.MinValue;
+                    }
+                    feedbackBusiness.BusinessPost = dr[18].ToString();
+                    if (!(dr[19] == null || dr[19].ToString().Length == 0))
+                    {
+                        feedbackBusiness.TakeStartDate = DateTime.Parse(dr[19].ToString());
+                    }
+                    else
+                    {
+                        feedbackBusiness.TakeStartDate = DateTime.MinValue;
+                    }
+                    if (!(dr[20] == null || dr[20].ToString().Length == 0))
+                    {
+                        feedbackBusiness.TakEndDate = DateTime.Parse(dr[20].ToString());
+                    }
+                    else
+                    {
+                        feedbackBusiness.TakEndDate = DateTime.MinValue;
+                    }
+                    feedbackBusiness.Scope = dr[21].ToString();
+                    feedbackBusiness.CreateTime = DateTime.Now.Date;
+                    feedbackBusinesses.Add(feedbackBusiness);
+                    if (i % 20 == 0)
+                    {
+                        backgroundWorker1.ReportProgress(0, "正在将以上20家企业信息保存至数据库。\r\n\r\n");
+                        using (BusinessContext context = new BusinessContext())
+                        {
+                            //context.Database.EnsureCreated();
+                            context.AddRange(feedbackBusinesses);
+                            context.SaveChanges();
+                        }
+                        feedbackBusinesses = new List<FeedbackBusiness>();
+                    }
                 }
-                else
+                if (feedbackBusinesses.Count > 0)
                 {
-                    feedbackBusiness.EstDate = DateTime.MinValue;
+                    using (BusinessContext context = new BusinessContext())
+                    {
+                        //context.Database.EnsureCreated();
+                        context.AddRange(feedbackBusinesses);
+                        context.SaveChanges();
+                    }
                 }
-                feedbackBusiness.LegalRep = dr[5].ToString();
-                feedbackBusiness.Residence = dr[6].ToString();
-                feedbackBusiness.State =dr[7].ToString();
-                if (dr[8].ToString().Equals("是"))
-                {
-                    feedbackBusiness.IsAbnormal = true;
-                }
-                else
-                {
-                    feedbackBusiness.IsAbnormal = false;
-                }
-                if (dr[9].ToString().Equals("是"))
-                {
-                    feedbackBusiness.IsOutrage = true;
-                }
-                else
-                {
-                    feedbackBusiness.IsOutrage = false;
-                }
-                if (!(dr[10] == null || dr[10].ToString().Length == 0))
-                {
-                    feedbackBusiness.RevokeDate = DateTime.Parse(dr[10].ToString());
-                }
-                else
-                {
-                    feedbackBusiness.RevokeDate = DateTime.MinValue;
-                }
-                if (!(dr[11] == null || dr[11].ToString().Length == 0))
-                {
-                    feedbackBusiness.LogoffDate = DateTime.Parse(dr[11].ToString());
-                }
-                else
-                {
-                    feedbackBusiness.LogoffDate = DateTime.MinValue;
-                }
-                feedbackBusiness.CreditCode = dr[12].ToString();
-                if (!(dr[13] == null || dr[13].ToString().Length == 0))
-                {
-                    feedbackBusiness.RegisteredCapital = decimal.Parse(dr[13].ToString());
-                }
-                else
-                {
-                    feedbackBusiness.RegisteredCapital = 0;
-                }
-                feedbackBusiness.Currency =dr[14].ToString();
-                if (!(dr[15] == null || dr[15].ToString().Length == 0))
-                {
-                    feedbackBusiness.Subscribe = decimal.Parse(dr[15].ToString());
-                }
-                else
-                {
-                    feedbackBusiness.Subscribe = 0;
-                }
-                if (!(dr[16] == null || dr[16].ToString().Length == 0))
-                {
-                    feedbackBusiness.Proportion = decimal.Parse(dr[16].ToString());
-                }
-                else
-                {
-                    feedbackBusiness.Proportion = 0;
-                }
-                if (!(dr[17] == null || dr[17].ToString().Length == 0))
-                {
-                    feedbackBusiness.PutupDate = DateTime.Parse(dr[17].ToString());
-                }
-                else
-                {
-                    feedbackBusiness.PutupDate = DateTime.MinValue;
-                }                
-                feedbackBusiness.BusinessPost = dr[18].ToString();
-                if (!(dr[19] == null || dr[19].ToString().Length == 0))
-                {
-                    feedbackBusiness.TakeStartDate = DateTime.Parse(dr[19].ToString());
-                }
-                else
-                {
-                    feedbackBusiness.TakeStartDate = DateTime.MinValue;
-                }
-                if (!(dr[20] == null || dr[20].ToString().Length == 0))
-                {
-                    feedbackBusiness.TakEndDate = DateTime.Parse(dr[20].ToString());
-                }
-                else
-                {
-                    feedbackBusiness.TakEndDate = DateTime.MinValue;
-                }
-                feedbackBusiness.Scope = dr[21].ToString();
-                feedbackBusiness.CreateTime = DateTime.Now.Date;
-                feedbackBusinesses.Add(feedbackBusiness);
-                
+                return true;
             }
-            return feedbackBusinesses;            
+            catch(Exception ex)
+            {
+                backgroundWorker1.ReportProgress(0, "出错：" + ex.Message + "\r\n\r\n");
+                return false;
+            }
+                    
         }
 
         private WorkUnit getCollectInfo(string fileName)
@@ -429,24 +459,30 @@ namespace EFCoreDemo
                 worker.ReportProgress(0, "正在导入" + fi.Name + "文件。\r\n\r\n");
                 if (fi.Name.IndexOf("工商反馈") > -1)
                 {
-                    List<FeedbackBusiness> feedbackBusinesses = getFeedbackInfO(fi.FullName);
-                    if (feedbackBusinesses.Count>0)
+                    if (getFeedbackInfo(fi.FullName))
                     {
-                        using (BusinessContext context = new BusinessContext())
-                        {                            
-                            //context.Database.EnsureCreated();
-                            context.AddRange(feedbackBusinesses);
-                            context.SaveChanges();
-                        }
-                        if (File.Exists(moveDir + fi.Name))
-                        {
-                            File.Delete(moveDir + fi.Name);
-                        }
-
-                        worker.ReportProgress(0,fi.Name+ "导入完成；\r\n\r\n");
+                        worker.ReportProgress(0, fi.Name + "导入完成；\r\n\r\n");
                         fi.MoveTo(moveDir + fi.Name);
                         worker.ReportProgress(0, "移动" + fi.Name + "文件至已分拣目录\r\n\r\n");
                     }
+                    //List<FeedbackBusiness> feedbackBusinesses = getFeedbackInfO(fi.FullName);
+                    //if (feedbackBusinesses.Count>0)
+                    //{
+                    //    using (BusinessContext context = new BusinessContext())
+                    //    {                            
+                    //        //context.Database.EnsureCreated();
+                    //        context.AddRange(feedbackBusinesses);
+                    //        context.SaveChanges();
+                    //    }
+                    //    if (File.Exists(moveDir + fi.Name))
+                    //    {
+                    //        File.Delete(moveDir + fi.Name);
+                    //    }
+
+                    //    worker.ReportProgress(0,fi.Name+ "导入完成；\r\n\r\n");
+                    //    fi.MoveTo(moveDir + fi.Name);
+                    //    worker.ReportProgress(0, "移动" + fi.Name + "文件至已分拣目录\r\n\r\n");
+                    //}
                     else
                     {
                         worker.ReportProgress(0, "导入" + fi.Name + "失败，请检查文件内容。\r\n\r\n");
@@ -502,6 +538,12 @@ namespace EFCoreDemo
         private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             textBox1.Text += e.UserState.ToString();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Select(textBox1.Text.Length, 0);
+            textBox1.ScrollToCaret();
         }
     }
     
